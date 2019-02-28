8786b04027bbfe478c5ae6c2bdfa7fb80666c8f3
Aleksiel Horak <captiveCorsair@Aleksiels-MacBook-Pro.local>
2019-02-28 21:56:22 +0200

    global variables

--- web calculator (javascript)/js/main.js
+++ web calculator (javascript)/js/main.js
@@ -1,2 +1,12 @@
 // calculator
 
+//Global vars
+
+var num1 = ""; //number prior to clicking operator button
+var num2 = ""; //number post clicking operator button
+var operator; 
+var flag = 0; //boolean zero = false. no operator button has been clicked
+ var display = document.getElementById("display"); //div in which our input will be displayed when we click on buttons
+ var equalTo = 0; //boolean zero = false. Used to reset display for new calculation
+
+
